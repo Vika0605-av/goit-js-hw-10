5ffd1618ddd449ed85f5b8ca9f4a3f0b14468adb
Виктория Александрова <you@example.com>
2026-03-09 20:33:40 +0100

    fix timer

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,27 +1,34 @@
 import flatpickr from "flatpickr"
 import "flatpickr/dist/flatpickr.min.css"
+import iziToast from "izitoast"
+import "izitoast/dist/css/iziToast.min.css"
+
 const startBtn = document.querySelector('button[data-start]');
 const daysEl = document.querySelector('span[data-days]');
 const hoursEl = document.querySelector('span[data-hours]');
 const minutesEl = document.querySelector('span[data-minutes]');
 const secondsEl = document.querySelector('span[data-seconds]');
+let userSelectedDate = null;
 let  intervalId;
+startBtn.disabled = true;
 startBtn.addEventListener('click', () => {
-    const initTime = new Date( '2026-08-25 09:00:00');
     intervalId = setInterval (() => {
         const futureTime = new Date();
-        const diff = initTime - futureTime;
-        const str = msToTime(diff);
+        const diff = userSelectedDate - futureTime;
         if (diff <= 0) {
             clearInterval(intervalId);
+            return;
         }
-        const days = Math.floor(diff / (1000 * 60 * 60 * 24));
-        const hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        const minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
-        const seconds = Math.floor((diff % (1000 * 60)) / 1000);
-        daysEl.textContent = days;
-        hoursEl.textContent = hours;
-        minutesEl.textContent = minutes;
-        secondsEl.textContent = seconds;
-    },1000);
+   function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+  const days = Math.floor(ms / day);
+  const hours = Math.floor((ms % day) / hour);
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+  return { days, hours, minutes, seconds };
+   }
+ }, 1000);
 });
