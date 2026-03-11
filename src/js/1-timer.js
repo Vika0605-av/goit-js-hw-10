2245a70b2711834896a8bb44534e821378b80e86
Виктория Александрова <you@example.com>
2026-03-11 16:29:05 +0100

    fix timer

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,7 +1,6 @@
-import flatpickr from "flatpickr"
-import "flatpickr/dist/flatpickr.min.css"
-import iziToast from "izitoast"
-import "izitoast/dist/css/iziToast.min.css"
+
+import "izipick/dist/izipick.min.css";
+import "flatpickr/dist/flatpickr.min.css";
 let userSelectedDate = null;
 let  intervalId;
 
