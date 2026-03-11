cee2fe8c31042a176b57c7fa675d14e3ee179763
Виктория Александрова <you@example.com>
2026-03-11 16:56:33 +0100

    fix js

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,6 +1,11 @@
 
-import "izipick/dist/izipick.min.css";
+
+// Описаний в документації
+import flatpickr from "flatpickr";
+// Додатковий імпорт стилів
 import "flatpickr/dist/flatpickr.min.css";
+
+
 let userSelectedDate = null;
 let  intervalId;
 
