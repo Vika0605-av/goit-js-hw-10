8664b7ed9310fe0b9cefdea6e0698bec16427795
Vika0605-av <polina031014@gmail.com>
2026-03-11 15:56:57 +0000

    Deploying to gh-pages from @ Vika0605-av/goit-js-hw-10@cee2fe8c31042a176b57c7fa675d14e3ee179763 🚀

---
+++ 2-snackbar.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */import"./assets/vendor-BlgSUhsj.js";function i(e,r,o){return new Promise((t,m)=>{setTimeout(()=>{o?t(e):m(e)},r)})}i("✅ Fulfilled promise in 1000ms",1e3,!0);i("❌ Rejected promise in 1000ms",1e3,!1);
+//# sourceMappingURL=2-snackbar.js.map
